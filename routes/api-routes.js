06aab40fb027630e4242e6fa535f6150fe83f59a
Danny Choi <choi.danny20@gmail.com>
2020-09-11 12:14:10 -0500

    added routes folder for api and html routes

---
+++ routes/api-routes.js
@@ -0,0 +1,52 @@
+const Workout = require("../models/workouts");
+
+module.exports = function (app) {
+  app.post("/api/workouts", (req, res) => {
+    Workout.create({}).then((data) => {
+      res.json(data);
+    });
+  });
+
+  app.put("/api/workouts/:id", ({ body, params }, res) => {
+    console.log(body);
+    Workout.findByIdAndUpdate(
+      params.id,
+      { $push: { exercises: body } },
+      { new: true, runValidators: true }
+    )
+      .then((data) => res.json(data))
+      .catch((err) => {
+        console.log(err);
+        res.json(err);
+      });
+  });
+
+  app.post("/api/workouts/range", (req, res) => {
+    Workout.create({})
+      .then((data) => res.json(data))
+      .catch((err) => {
+        res.json(err);
+      });
+  });
+
+  app.get("/api/workouts", (req, res) => {
+    Workout.find()
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
+
+  app.get("/api/workouts/range", (req, res) => {
+    Workout.findOne({})
+      .limit(7)
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
+};
